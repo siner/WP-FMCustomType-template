c7e4c8d95e0aba9df9c17c9f868d67586787698d
Fran Moreno <fran.siner@gmail.com>
2012-01-11 15:18:06 +0100

    Added more kind of fields and doing more generic and easy

--- WP-CustomType.php
+++ WP-CustomType.php
@@ -99,38 +99,197 @@ function ctt_custom_columns($column){
 
 /* Meta Boxes */
 
-add_action( 'admin_init' , 'ctt_admin_init');
- 
-function ctt_admin_init(){
-  add_meta_box( 'example-meta', 'Example', 'ctt_example', CUSTOM_TYPE_SLUG , 'normal', 'low');
+
+// Add the Meta Box
+function ctt_add_meta_box() {
+    add_meta_box(
+		'ctt_meta_box', // $id
+		CUSTOM_TYPE_SINGULAR_NAME . ' Meta Box', // $title
+		'ctt_show_meta_box', // $callback
+		CUSTOM_TYPE_SLUG, // $page
+		'normal', // $context
+		'high'); // $priority
 }
- 
-function ctt_example(){
-  global $post;
-  $custom = get_post_custom($post->ID);
-  $example = sizeof($custom)>0 ? $custom["tests_example"][0] : '';
-  ?>
-  <p><label>Example:</label><br/>
-  <textarea cols="50" rows="5" name="tests_example"><?php echo $example; ?></textarea></p>
-  <?php
+add_action('add_meta_boxes', 'ctt_add_meta_box');
+
+// Field Array
+$prefix = CUSTOM_TYPE_SLUG . '_';
+$custom_meta_fields = array(
+	array(
+		'label'=> 'Text Input',
+		'desc'	=> 'A description for the field.',
+		'id'	=> $prefix.'text',
+		'type'	=> 'text'
+	),
+	array(
+		'label'=> 'Textarea',
+		'desc'	=> 'A description for the field.',
+		'id'	=> $prefix.'textarea',
+		'type'	=> 'textarea'
+	),
+	array(
+		'label'=> 'Checkbox Input',
+		'desc'	=> 'A description for the field.',
+		'id'	=> $prefix.'checkbox',
+		'type'	=> 'checkbox'
+	),
+	array(
+		'label'=> 'Select Box',
+		'desc'	=> 'A description for the field.',
+		'id'	=> $prefix.'select',
+		'type'	=> 'select',
+		'options' => array (
+			'one' => array (
+				'label' => 'Option One',
+				'value'	=> 'one'
+			),
+			'two' => array (
+				'label' => 'Option Two',
+				'value'	=> 'two'
+			),
+			'three' => array (
+				'label' => 'Option Three',
+				'value'	=> 'three'
+			)
+		)
+	),
+	array (  
+    'label' => 'Radio Group',  
+    'desc'  => 'A description for the field.',  
+    'id'    => $prefix.'radio',  
+    'type'  => 'radio',  
+    'options' => array (  
+        'one' => array (  
+            'label' => 'Option One',  
+            'value' => 'one'  
+        ),  
+        'two' => array (  
+            'label' => 'Option Two',  
+            'value' => 'two'  
+        ),  
+        'three' => array (  
+            'label' => 'Option Three',  
+            'value' => 'three'  
+        )  
+    )  
+	),
+	array (  
+    'label' => 'Checkbox Group',  
+    'desc'  => 'A description for the field.',  
+    'id'    => $prefix.'checkbox_group',  
+    'type'  => 'checkbox_group',  
+    'options' => array (  
+        'one' => array (  
+            'label' => 'Option One',  
+            'value' => 'one'  
+        ),  
+        'two' => array (  
+            'label' => 'Option Two',  
+            'value' => 'two'  
+        ),  
+        'three' => array (  
+            'label' => 'Option Three',  
+            'value' => 'three'  
+        )  
+    )  
+	)
+
+);
+
+
+
+
+// The Callback
+function ctt_show_meta_box() {
+global $custom_meta_fields, $post;
+// Use nonce for verification
+echo '<input type="hidden" name="custom_meta_box_nonce" value="'.wp_create_nonce(basename(__FILE__)).'" />';
+
+	// Begin the field table and loop
+	echo '<table class="form-table">';
+	foreach ($custom_meta_fields as $field) {
+		// get value of this field if it exists for this post
+		$meta = get_post_meta($post->ID, $field['id'], true);
+		// begin a table row with
+		echo '<tr>
+				<th><label for="'.$field['id'].'">'.$field['label'].'</label></th>
+				<td>';
+				switch($field['type']) {
+					// text
+					case 'text':  
+				    echo '<input type="text" name="'.$field['id'].'" id="'.$field['id'].'" value="'.$meta.'" size="30" /> 
+			        		<br /><span class="description">'.$field['desc'].'</span>';  
+						break; 
+					// textarea  
+					case 'textarea':  
+					    echo '<textarea name="'.$field['id'].'" id="'.$field['id'].'" cols="60" rows="4">'.$meta.'</textarea> 
+					        <br /><span class="description">'.$field['desc'].'</span>';  
+					break; 
+					// checkbox  
+					case 'checkbox':  
+					    echo '<input type="checkbox" name="'.$field['id'].'" id="'.$field['id'].'" ',$meta ? ' checked="checked"' : '','/> ';
+ 					    echo '<span class="description">'.$field['desc'].'</span>';   
+					break; 
+					// select  
+					case 'select':  
+					    echo '<select name="'.$field['id'].'" id="'.$field['id'].'">';  
+					    foreach ($field['options'] as $option) {  
+					        echo '<option', $meta == $option['value'] ? ' selected="selected"' : '', ' value="'.$option['value'].'">'.$option['label'].'</option>';  
+					    }  
+					    echo '</select><br /><span class="description">'.$field['desc'].'</span>';  
+					break;
+					// radio  
+					case 'radio':  
+					    foreach ( $field['options'] as $option ) {  
+					        echo '<input type="radio" name="'.$field['id'].'" id="'.$option['value'].'" value="'.$option['value'].'" ',$meta == $option['value'] ? ' checked="checked"' : '',' /> 
+					                <label for="'.$option['value'].'">'.$option['label'].'</label><br />';  
+					    }  
+					    echo '<span class="description">'.$field['desc'].'</span>';  
+					break;
+					// checkbox_group  
+					case 'checkbox_group':  
+					    foreach ($field['options'] as $option) {  
+					        echo '<input type="checkbox" value="'.$option['value'].'" name="'.$field['id'].'[]" id="'.$option['value'].'"',$meta && in_array($option['value'], $meta) ? ' checked="checked"' : '',' /> 
+					                <label for="'.$option['value'].'">'.$option['label'].'</label><br />';  
+					    }  
+					    echo '<span class="description">'.$field['desc'].'</span>';  
+					break;
+				} //end switch
+		echo '</td></tr>';
+	} // end foreach
+	echo '</table>'; // end table
 }
 
 
-add_action('save_post', 'ctt_save_details');
-
-function ctt_save_details(){
-  global $post;
-
-	if (!is_array($post) && $post->post_type == CUSTOM_TYPE_SLUG)
-	{
-		$custom_meta_fields = array( CUSTOM_TYPE_SLUG . '_example' );
-		
-		foreach( $custom_meta_fields as $custom_meta_field ):
-		if(isset($_POST[$custom_meta_field]) && $_POST[$custom_meta_field] != ""):
-			update_post_meta($post->ID, $custom_meta_field, $_POST[$custom_meta_field]);
-		endif;
-		endforeach;
-	}
-}
+// Save the Data  
+function ctt_save_meta($post_id) {  
+    global $custom_meta_fields;  
+  
+    // verify nonce  
+    if (!wp_verify_nonce($_POST['custom_meta_box_nonce'], basename(__FILE__)))  
+        return $post_id;  
+    // check autosave  
+    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE)  
+        return $post_id;  
+    // check permissions  
+    if ('page' == $_POST['post_type']) {  
+        if (!current_user_can('edit_page', $post_id))  
+            return $post_id;  
+        } elseif (!current_user_can('edit_post', $post_id)) {  
+            return $post_id;  
+    }  
+  
+    // loop through fields and save the data  
+    foreach ($custom_meta_fields as $field) {  
+        $old = get_post_meta($post_id, $field['id'], true);  
+        $new = $_POST[$field['id']];  
+        if ($new && $new != $old) {  
+            update_post_meta($post_id, $field['id'], $new);  
+        } elseif ('' == $new && $old) {  
+            delete_post_meta($post_id, $field['id'], $old);  
+        }  
+    } // end foreach  
+}  
+add_action('save_post', 'ctt_save_meta');
 
 ?>
